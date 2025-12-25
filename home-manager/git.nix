61d39a229cca64aaf4df01af1e6935bd886e56b9
Hamtak0 <motorfirst2547@gmail.com>
2025-12-25 10:15:04 +0700

    fix: default user and email for git

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -1,10 +1,15 @@
-{...}: {
+{ ... }:
+{
   programs.git = {
     enable = true;
     settings = {
+      user = {
+        name = "Hamtak0";
+        email = "motorfirst2547@gmail.com";
+      };
       init.defaultBranch = "main";
       core = {
-	sshCommand = "ssh -o IdentitiesOnly=yes -i ~/.ssh/id_ed25519.github.Hamtak0";
+        sshCommand = "ssh -o IdentitiesOnly=yes -i ~/.ssh/id_ed25519.github.Hamtak0";
       };
     };
   };
