7b0528bd3b4f43a88c4b300ceb3cf0b8e8b8b837
Hamtak0 <motorfirst2547@gmail.com>
2026-01-05 14:08:30 +0700

    feat!: change tuigreet to sddm

--- nixos/greeter.nix
+++ nixos/greeter.nix
@@ -1,32 +1,44 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  ...
+}:
 let
-  tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
+  #tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
   hyprland-session = "${pkgs.hyprland}/share/wayland-sessions";
 in
 {
+  # tuigreet settings
   services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session}";
-        user = "greeter";
-      };
-    };
+    enable = false;
+    #settings = {
+    #  default_session = {
+    #    command = "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session}";
+    #    user = "greeter";
+    #  };
+    #};
   };
   # this is a life saver.
   # literally no documentation about this anywhere.
   # might be good to write about this...
   # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
-  systemd.services.greetd.serviceConfig = {
-    Type = "idle";
-    StandardInput = "tty";
-    StandardOutput = "tty";
-    StandardError = "journal"; # Without this errors will spam on screen
-    # Without these bootlogs will spam on screen
-    TTYReset = true;
-    TTYVHangup = true;
-    TTYVTDisallocate = true;
+  #systemd.services.greetd.serviceConfig = {
+  #  Type = "idle";
+  #  StandardInput = "tty";
+  #  StandardOutput = "tty";
+  #  StandardError = "journal"; # Without this errors will spam on screen
+  #  # Without these bootlogs will spam on screen
+  #  TTYReset = true;
+  #  TTYVHangup = true;
+  #  TTYVTDisallocate = true;
 
-    # services.displayManager.sddm.wayland.enable = true;
+  #};
+
+  # sddm settings
+  services.xserver.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
   };
+
 }
