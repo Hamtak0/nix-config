38be2aa9fe3462ef40bc62a7d53a1f732d0114b2
Hamtak0 <motorfirst2547@gmail.com>
2026-01-05 14:27:22 +0700

    feat: hyprland cachix

--- home-manager/programs/hyprland/default.nix
+++ home-manager/programs/hyprland/default.nix
@@ -4,6 +4,14 @@
   ...
 }:
 {
+  nix.settings = {
+    extra-substituters = [ "https://hyprland.cachix.org" ];
+    extra-trusted-substituters = [ "https://hyprland.cachix.org" ];
+    extra-trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
     package = null;
