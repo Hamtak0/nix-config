7f4e1fcbecb8fef1c85e398c37de165ab6393d21
Hamtak0 <motorfirst2547@gmail.com>
2026-01-29 14:32:48 +0700

    feat: add abbreviation mh and mc

--- home-manager/programs/fish.nix
+++ home-manager/programs/fish.nix
@@ -7,6 +7,8 @@
       ni = "nh os switch /etc/nix-config";
       y = "yazi";
       cdn = "cd /etc/nix-config";
+      mh = "man home-configuration.nix";
+      mc = "man configuration.nix";
     };
   };
 }
