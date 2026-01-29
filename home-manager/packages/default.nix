cae8b97bcb77a1ad5dca9bfb6a3dfdb430a7a8dc
Hamtak0 <motorfirst2547@gmail.com>
2026-01-29 14:36:56 +0700

    refactor: packages to programs

--- home-manager/packages/default.nix
+++ home-manager/packages/default.nix
@@ -1,17 +1,12 @@
 {
   imports = [
-    ./anki.nix
     ./brave.nix
-    ./discord.nix
     ./devenv.nix
     ./grimblast.nix
     ./imagemagick.nix
     ./libreoffice.nix
-    # ./minicom.nix # Modem control and terminal emulation program
     ./nixfmt.nix
-    ./nix-index.nix
     ./pavucontrol.nix
-    ./ripgrep.nix
     ./steam.nix
     ./wireshark.nix
   ];
--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -1,12 +1,16 @@
 {
   imports = [
+    ./anki.nix
+    ./discord.nix
     ./fastfetch.nix
     ./fish.nix
     ./git.nix
     ./hyprland
     ./hyprlock.nix
-    # ./nixvim.nix
     ./nh.nix
+    ./nix-index.nix
+    # ./nixvim.nix
+    ./ripgrep.nix
     ./rofi.nix
     ./yazi.nix
     ./zed.nix
