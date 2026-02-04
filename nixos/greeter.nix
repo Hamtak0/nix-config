d88f997ec0d859edbeff2daf34c2b85e0d4b2cdc
Hamtak0 <motorfirst2547@gmail.com>
2026-02-04 22:18:04 +0700

    feat: sakura theme for sddm greeter

--- nixos/greeter.nix
+++ nixos/greeter.nix
@@ -5,9 +5,35 @@
 }:
 let
   #tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
-  hyprland-session = "${pkgs.hyprland}/share/wayland-sessions";
+  #hyprland-session = "${pkgs.hyprland}/share/wayland-sessions";
+
+  custom-sddm-astronaut = pkgs.sddm-astronaut.override {
+    embeddedTheme = "pixel_sakura";
+  };
 in
 {
+  # sddm settings
+  services.xserver.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+
+    enableHidpi = true;
+    autoNumlock = true;
+    theme = "sddm-astronaut-theme";
+    settings = {
+      Theme = {
+        Current = "sddm-astronaut-theme";
+      };
+    };
+    extraPackages = [ custom-sddm-astronaut ];
+  };
+
+  environment.systemPackages = [
+    custom-sddm-astronaut
+    pkgs.kdePackages.qtmultimedia
+  ];
+
   # tuigreet settings
   #services.greetd = {
   #  enable = true;
@@ -32,12 +58,4 @@ in
   #  TTYVHangup = true;
   #  TTYVTDisallocate = true;
   #};
-
-  # sddm settings
-  services.xserver.enable = true;
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-  };
-
 }
