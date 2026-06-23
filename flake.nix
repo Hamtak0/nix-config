819fe45ebf651357ef28fd92cea4e0aef260a44e
Hamtak0 <motorfirst2547@gmail.com>
2026-06-23 11:31:41 +0700

    chore: flake update

--- flake.nix
+++ flake.nix
@@ -15,10 +15,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # nixvim = {
+    #   url = "github:nix-community/nixvim";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     hyprland.url = "github:hyprwm/Hyprland";
     hyprland-plugins = {
