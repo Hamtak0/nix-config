63acdfa1a92cbc147327a76a5861b9b16b92e0cc
Hamtak0 <motorfirst2547@gmail.com>
2026-07-02 09:55:55 +0700

    fix: session doesn't start up after suspend

--- home-manager/services/hypridle.nix
+++ home-manager/services/hypridle.nix
@@ -5,7 +5,7 @@
       general = {
         lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
         before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
-        after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
+        after_sleep_cmd = "sleep 3 && hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
         # The general:inhibit_sleep option is used to make sure hypridle can perform certain tasks before the system goes to sleep.
         # 0 -> disables sleep inhibition.
         # 1 -> makes the system wait until hypridle launched general:before_sleep_cmd.
@@ -17,19 +17,23 @@
       listener = [
         {
           # turn off keyboard backlight, delete out the section if you dont have a keyboard backlight.
-          timeout = 180; # 3min.
+          timeout = 180; # 3minutes.
           on-timeout = "brightnessctl -s set 15 && brightnessctl -sd asus::kbd_backlight set 0"; # set monitor backlight to minimum, avoid 0 on OLED monitor and turn off keyboard backlight.
           on-resume = "brightnessctl -r && brightnessctl -rd asus::kbd_backlight"; # monitor backlight restore and turn on keyboard backlight.
         }
         {
-          timeout = 300; # 5min.
-          on-timeout = "loginctl lock-session && hyprctl dispatch dpms off"; # lock screen and screen off when timeout has passed.
+          timeout = 300; # 5minutes.
+          on-timeout = "loginctl lock-session"; # lock screen
+        }
+        {
+          timeout = 330; # 5.5minutes.
+          on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed.
           on-resume = "hyprctl dispatch dpms on && brightnessctl -r"; # screen on when activity is detected after timeout has fired.
         }
         # The "systemctl suspend" is buggy (black screen while the keyboard and fan is working), if the supergfx didn't set to Hybrid.
         {
           timeout = 3600; # 1hour.
-          on-timeout = "playerctl status 2>/dev/null | grep -q 'Playing' || systemctl suspend"; # suspend pc if no music is playing in the background.
+          on-timeout = "playerctl status 2>/dev/null | grep -q 'Playing' || (hyprctl dispatch dpms on && sleep 3 && systemctl suspend)"; # suspend pc if no music is playing in the background.
         }
         {
           timeout = 7200; # 2hours.
