e59736c789ad816c1f2dda02631984437bf2afe3
Hamtak0 <motorfirst2547@gmail.com>
2026-07-12 18:37:22 +0700

    chore: support system flake exposed

--- flake.nix
+++ flake.nix
@@ -46,16 +46,17 @@
     let
       inherit (self) outputs;
       # Supported systems for your flake packages, shell, etc.
-      systems = [
-        "aarch64-linux"
-        "i686-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
+      # systems = [
+      #   "aarch64-linux"
+      #   "i686-linux"
+      #   "x86_64-linux"
+      #   "aarch64-darwin"
+      #   "x86_64-darwin"
+      # ];
       # This is a function that generates an attribute by calling a function you
       # pass to it, with each system as an argument
-      forAllSystems = nixpkgs.lib.genAttrs systems;
+      # forAllSystems = nixpkgs.lib.genAttrs systems;
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in
     {
       # Your custom packages
