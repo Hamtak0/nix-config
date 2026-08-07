b4cb368209b2d09efe4aa61d3e6c3bbb7283f52a
Hamtak0 <motorfirst2547@gmail.com>
2026-08-07 09:40:55 +0700

    fix: gnome grc-ssh-agent to ssh-agent

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -33,6 +33,7 @@
     ./locale.nix
     ./pam.nix
     ./sound.nix
+    ./ssh.nix
     ./steam.nix
     ./time.nix
     ./users.nix
@@ -119,19 +120,6 @@
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
-
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Opinionated: forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Remove if you want to SSH using passwords
-      PasswordAuthentication = false;
-    };
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
--- nixos/services/gnome-keyring.nix
+++ nixos/services/gnome-keyring.nix
@@ -4,4 +4,7 @@
 
   programs.seahorse.enable = true;
   programs.dconf.enable = true;
+
+  # fix ssh overlapped
+  services.gnome.gcr-ssh-agent.enable = false;
 }
