db1457ad657cb01c1877f37dd9cb00b20688441f
Hamtak0 <motorfirst2547@gmail.com>
2026-08-15 11:23:53 +0700

    feat: postgresql backup

--- nixos/services/postgresql.nix
+++ nixos/services/postgresql.nix
@@ -5,28 +5,35 @@ let
   );
 in
 {
-  services.postgresql = {
-    enable = true;
-    ensureDatabases = [ "mydatabase" ] ++ usernames;
-    ensureUsers = map (username: {
-      name = username;
-      ensureClauses = {
-        # superuser = true;
-        login = true;
-        createdb = true;
-      };
-    }) usernames;
+  services = {
+    postgresql = {
+      enable = true;
+      ensureDatabases = [ "mydatabase" ] ++ usernames;
+      ensureUsers = map (username: {
+        name = username;
+        ensureClauses = {
+          # superuser = true;
+          login = true;
+          createdb = true;
+        };
+      }) usernames;
 
-    authentication = pkgs.lib.mkOverride 10 ''
-      # type  database  DBuser  auth-method optional_ident_map
-        local all       all     peer        map=superuser_map
-    '';
-    identMap = ''
-      # ArbitraryMapName  systemUser  DBUser
-        superuser_map     root        postgres
-        superuser_map     postgres    postgres
-        # Let other names login as themselves
-        superuser_map     /^(.*)$     \1
-    '';
+      authentication = pkgs.lib.mkOverride 10 ''
+        # type  database  DBuser  auth-method optional_ident_map
+          local all       all     peer        map=superuser_map
+      '';
+      identMap = ''
+        # ArbitraryMapName  systemUser  DBUser
+          superuser_map     root        postgres
+          superuser_map     postgres    postgres
+          # Let other names login as themselves
+          superuser_map     /^(.*)$     \1
+      '';
+    };
+    postgresqlBackup = {
+      enable = true;
+      databases = [ "mydatabase" ] ++ usernames;
+      startAt = "*-*-* 04:00:00";
+    };
   };
 }
