0b4137c3ac41fba04917bec6c38559a62cc776ad
Hamtak0 <motorfirst2547@gmail.com>
2026-08-15 11:11:37 +0700

    feat: postgresql database service

--- nixos/services/default.nix
+++ nixos/services/default.nix
@@ -4,6 +4,7 @@
     ./gnome-keyring.nix
     # ./hermes-agent.nix
     # ./open-webui.nix
+    ./postgresql.nix
     ./power-profiles-daemon.nix
     ./resolved.nix
     ./tailscale.nix
---
+++ nixos/services/postgresql.nix
@@ -0,0 +1,32 @@
+{ pkgs, config, ... }:
+let
+  usernames = builtins.attrNames (
+    pkgs.lib.filterAttrs (_: user: user.isNormalUser) config.users.users
+  );
+in
+{
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "mydatabase" ] ++ usernames;
+    ensureUsers = map (username: {
+      name = username;
+      ensureClauses = {
+        # superuser = true;
+        login = true;
+        createdb = true;
+      };
+    }) usernames;
+
+    authentication = pkgs.lib.mkOverride 10 ''
+      # type  database  DBuser  auth-method optional_ident_map
+        local all       all     peer        map=superuser_map
+    '';
+    identMap = ''
+      # ArbitraryMapName  systemUser  DBUser
+        superuser_map     root        postgres
+        superuser_map     postgres    postgres
+        # Let other names login as themselves
+        superuser_map     /^(.*)$     \1
+    '';
+  };
+}
