83a27aaa4c9bb564d6658d11848f14e1d0e79c6c
Hamtak0 <motorfirst2547@gmail.com>
2026-09-05 11:44:44 +0700

    fix(idle): suspend with no loading session

--- home-manager/services/hypridle.nix
+++ home-manager/services/hypridle.nix
@@ -12,7 +12,7 @@
         general = {
           lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
           before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
-          after_sleep_cmd = dpms_on; # to avoid having to press a key twice to turn on the display.
+          after_sleep_cmd = "sleep 3 && ${dpms_on}"; # to avoid having to press a key twice to turn on the display.
           # The general:inhibit_sleep option is used to make sure hypridle can perform certain tasks before the system goes to sleep.
           # 0 -> disables sleep inhibition.
           # 1 -> makes the system wait until hypridle launched general:before_sleep_cmd.
